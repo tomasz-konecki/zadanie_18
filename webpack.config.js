a5276365fefc2d422726b14aafd77620711a0af6
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-10-22 20:49:06 +0200

    Update repo

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    UglifyJSPlugin = require('uglifyjs-webpack-plugin'),
+    OptimizeJsPlugin = require('optimize-js-plugin'),
+
+    env = process.env.NODE_ENV || 'development',
+
+    plugins = [new HtmlWebpackPlugin({
+        template: './src/index.ejs',
+        filename: 'index.html',
+        inject: 'body'
+        })
+    ];
+
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+        new OptimizeJsPlugin({
+            sourceMap: false
+        })
+    );
+}
+
+module.exports = {
+    entry: (env !== 'production' ? [
+            'react-hot-loader/patch',
+            'webpack-dev-server/client?http://localhost:8080',
+            'webpack/hot/only-dev-server',
+        ] : []).concat(['./client/index.js']),
+    output: {
+        filename: './bundle.js',
+        path: resolve(__dirname, 'public'),
+    }
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: __dirname + '/node_modules'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: plugins
+}
